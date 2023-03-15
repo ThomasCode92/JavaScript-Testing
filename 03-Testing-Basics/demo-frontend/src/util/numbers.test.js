949a99d3e90446a369fcfde05d96a4c2119358ac
ThomasCode92 <thomascode92@hotmail.com>
2023-03-15 19:37:07 +0100

    Tests with Multiple Assertions (Multiple Expectations)

--- 03-Testing-Basics/demo-frontend/src/util/numbers.test.js
+++ 03-Testing-Basics/demo-frontend/src/util/numbers.test.js
@@ -4,16 +4,21 @@ import { transformToNumber } from './numbers';
 
 it('should transform a string number to a number of type number', () => {
   const input = '1';
+  const expectedResult = parseInt(input);
 
   const result = transformToNumber(input);
 
   expect(result).toBeTypeOf('number');
+  expect(result).toBe(expectedResult);
 });
 
 it('should yield NaN for non-transformable values', () => {
-  const input = 'invalid';
+  const input1 = 'invalid';
+  const input2 = {};
 
-  const result = transformToNumber(input);
+  const result1 = transformToNumber(input1);
+  const result2 = transformToNumber(input2);
 
-  expect(result).toBeNaN();
+  expect(result1).toBeNaN();
+  expect(result2).toBeNaN();
 });
