572930c87ec42caa3fba53a537d38a0d058f490a
ThomasCode92 <thomascode92@hotmail.com>
2023-03-15 21:00:28 +0100

    Writing an Integration Test & Reasoning

--- 05-Integration-Tests/demo-frontend/src/util/numbers.test.js
+++ 05-Integration-Tests/demo-frontend/src/util/numbers.test.js
@@ -1,24 +1,37 @@
-import { expect, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
-import { transformToNumber } from './numbers';
+import { cleanNumbers, transformToNumber } from './numbers';
 
-it('should transform a string number to a number of type number', () => {
-  const input = '1';
-  const expectedResult = parseInt(input);
+describe('transformNumber()', () => {
+  it('should transform a string number to a number of type number', () => {
+    const input = '1';
+    const expectedResult = parseInt(input);
 
-  const result = transformToNumber(input);
+    const result = transformToNumber(input);
 
-  expect(result).toBeTypeOf('number');
-  expect(result).toBe(expectedResult);
+    expect(result).toBeTypeOf('number');
+    expect(result).toBe(expectedResult);
+  });
+
+  it('should yield NaN for non-transformable values', () => {
+    const input1 = 'invalid';
+    const input2 = {};
+
+    const result1 = transformToNumber(input1);
+    const result2 = transformToNumber(input2);
+
+    expect(result1).toBeNaN();
+    expect(result2).toBeNaN();
+  });
 });
 
-it('should yield NaN for non-transformable values', () => {
-  const input1 = 'invalid';
-  const input2 = {};
+describe('cleanNumbers()', () => {
+  // This can be called an 'Integration Test'
+  it('should return an array of number values if an array of string number valuesis provided', () => {
+    const numberValues = ['1', '2'];
 
-  const result1 = transformToNumber(input1);
-  const result2 = transformToNumber(input2);
+    const cleanedNumbers = cleanNumbers(numberValues);
 
-  expect(result1).toBeNaN();
-  expect(result2).toBeNaN();
+    expect(cleanedNumbers[0]).toBeTypeOf('number');
+  });
 });
