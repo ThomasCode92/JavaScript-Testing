c4de7618ef9c5e1f71eed528b914a420571f57ea
ThomasCode92 <thomascode92@hotmail.com>
2023-03-16 08:15:42 +0100

    Testing Asynchronous Code With Callbacks

--- 06-Advanced-Concepts/demo-async-hooks/async/async.test.js
+++ 06-Advanced-Concepts/demo-async-hooks/async/async.test.js
@@ -2,11 +2,17 @@ import { expect, it } from 'vitest';
 
 import { generateToken } from './async';
 
-it('should generate a token value', () => {
-  const testUserEmail = 'test@test.com';
+it('should generate a token value', () =>
+  new Promise((resolve, reject) => {
+    const testUserEmail = 'test@test.com';
 
-  generateToken(testUserEmail, (err, token) => {
-    // expect(token).toBeDefined();
-    expect(token).toBe(2);
-  });
-});
+    generateToken(testUserEmail, (err, token) => {
+      try {
+        expect(token).toBeDefined();
+        // expect(token).toBe(2);
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }));
