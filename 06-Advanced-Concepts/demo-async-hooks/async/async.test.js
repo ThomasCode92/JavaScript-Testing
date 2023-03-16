76f488f7f8941b7eaf33c44f8c4d1d0eaa5c878c
ThomasCode92 <thomascode92@hotmail.com>
2023-03-16 08:04:56 +0100

    The Problem With Asynchronous Code

---
+++ 06-Advanced-Concepts/demo-async-hooks/async/async.test.js
@@ -0,0 +1,12 @@
+import { expect, it } from 'vitest';
+
+import { generateToken } from './async';
+
+it('should generate a token value', () => {
+  const testUserEmail = 'test@test.com';
+
+  generateToken(testUserEmail, (err, token) => {
+    // expect(token).toBeDefined();
+    expect(token).toBe(2);
+  });
+});
