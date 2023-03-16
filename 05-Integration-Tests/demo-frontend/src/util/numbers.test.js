66112e7c19665632402193d82a550e073cf3e050
ThomasCode92 <thomascode92@hotmail.com>
2023-03-16 07:39:16 +0100

    Integration Tests

---
+++ 05-Integration-Tests/demo-frontend/src/util/numbers.test.js
@@ -0,0 +1,45 @@
+import { describe, expect, it } from 'vitest';
+
+import { cleanNumbers, transformToNumber } from './numbers';
+
+describe('transformNumber()', () => {
+  it('should transform a string number to a number of type number', () => {
+    const input = '1';
+    const expectedResult = parseInt(input);
+
+    const result = transformToNumber(input);
+
+    expect(result).toBeTypeOf('number');
+    expect(result).toBe(expectedResult);
+  });
+
+  it('should yield NaN for non-transformable values', () => {
+    const input1 = 'invalid';
+    const input2 = {};
+
+    const result1 = transformToNumber(input1);
+    const result2 = transformToNumber(input2);
+
+    expect(result1).toBeNaN();
+    expect(result2).toBeNaN();
+  });
+});
+
+describe('cleanNumbers()', () => {
+  // This can be called an 'Integration Test'
+  it('should return an array of number values if an array of string number valuesis provided', () => {
+    const numberValues = ['1', '2'];
+
+    const cleanedNumbers = cleanNumbers(numberValues);
+
+    expect(cleanedNumbers[0]).toBeTypeOf('number');
+  });
+
+  it('should throw an error if an error with at least one empty string is provided', () => {
+    const numberValues = ['', 1];
+
+    const cleanFn = () => cleanNumbers(numberValues);
+
+    expect(cleanFn).toThrow();
+  });
+});
