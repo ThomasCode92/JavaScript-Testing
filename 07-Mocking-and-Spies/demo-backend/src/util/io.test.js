58d81f5c10234702cc64526dd56ac579e303551c
ThomasCode92 <thomascode92@hotmail.com>
2023-03-20 07:58:57 +0100

    Getting Started with Mocks & Automocking

--- 07-Mocking-and-Spies/demo-backend/src/util/io.test.js
+++ 07-Mocking-and-Spies/demo-backend/src/util/io.test.js
@@ -1,10 +1,15 @@
-import { it, expect } from 'vitest';
+import { promises as fs } from 'fs';
+import { it, expect, vi } from 'vitest';
 
 import writeData from './io';
+
+vi.mock('fs');
 
 it('should execute the writeFile method', () => {
   const testData = 'Test';
   const testFilename = 'test.txt';
 
-  expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+  writeData(testData, testFilename);
+
+  expect(fs.writeFile).toBeCalled();
 });
