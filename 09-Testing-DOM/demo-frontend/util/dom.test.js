f848697ea809d6195f19cd55ff35b966c4353eec
ThomasCode92 <thomascode92@hotmail.com>
2023-03-22 08:19:47 +0100

    Setting up a Virtual HTML Page

--- 09-Testing-DOM/demo-frontend/util/dom.test.js
+++ 09-Testing-DOM/demo-frontend/util/dom.test.js
@@ -1,6 +1,20 @@
-import { it } from 'vitest';
+import path from 'path';
+import fs from 'fs';
+
+import { it, vi } from 'vitest';
+import { Window } from 'happy-dom';
 
 import { showError } from './dom';
+
+const htmlDocPath = path.join(process.cwd(), 'index.html');
+const htmlDocContent = fs.readFileSync(htmlDocPath).toString();
+
+const window = new Window();
+const document = window.document;
+
+document.write(htmlDocContent);
+
+vi.stubGlobal('document', document);
 
 it('first test', () => {
   showError('test');
