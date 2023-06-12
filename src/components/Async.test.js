ef4208dba29c69c817fc3fe0bec7a51cdc95bdab
ThomasCode92 <thomascode92@hotmail.com>
2023-06-12 08:13:46 +0200

    Working with Mocks

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,6 +3,11 @@ import Async from './Async';
 
 describe('Async component', () => {
   test('should render posts if request succeeds', async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: 1, title: 'First Post' }],
+    });
+
     render(<Async />);
 
     const listItemElements = await screen.findAllByRole('listitem');
